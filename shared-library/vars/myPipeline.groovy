b9865304e720135ff04480acf4afc682418fafa5
samitkumarpatel <samitkumarpatel@gmail.com>
2021-02-21 19:55:27 +0100

    some more shared library related testing

---
+++ shared-library/vars/myPipeline.groovy
@@ -0,0 +1,11 @@
+node {
+    stage('01') {
+        echo "01"
+    }
+    stage('02') {
+        echo "02"
+    }
+    stage('03') {
+        echo "03"
+    }
+}
---
+++ shared-library/vars/oneToTen.groovy
@@ -0,0 +1,5 @@
+def call() {
+    ​[1..10].each {
+        println "${it}"
+    }​
+}
