0bf3737e61634b4b8baf5cf414a94aa5332c9da4
samitkumarpatel <samitkumarpatel@gmail.com>
2021-02-21 20:07:24 +0100

    update

--- shared-library/vars/myPipeline.groovy
+++ shared-library/vars/myPipeline.groovy
@@ -1,11 +1,10 @@
-node {
-    stage('01') {
-        echo "01"
-    }
-    stage('02') {
-        echo "02"
-    }
-    stage('03') {
-        echo "03"
-    }
+
+stage('01') {
+    echo "01"
+}
+stage('02') {
+    echo "02"
+}
+stage('03') {
+    echo "03"
 }
